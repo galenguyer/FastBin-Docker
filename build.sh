cdf96ddb5b39d74afa420ac68a3102a544949159
Galen Guyer <git@galenguyer.com>
2020-05-16 01:47:39 -0700

    Add web files

--- build.sh
+++ build.sh
@@ -22,6 +22,7 @@ docker run -it --rm -e "NGINX=$version" -v "$basedir"/artifacts:/build alpine:la
 # copy binaries to image build directory
 cp "$basedir"/artifacts/nginx-"$version" "$basedir"/image/nginx
 cp -r "$basedir"/artifacts/server "$basedir"/image/server
+git clone https://github.com/galenguyer/fastbin-web "$basedir"/images/web
 
 # create docker run image
 docker build --build-arg version="$version" -t fastbin:latest "$basedir"/image/.
