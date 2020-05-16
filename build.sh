7d33ccd7c196be48d9063db64d1092b26f383f74
Galen Guyer <git@galenguyer.com>
2020-05-16 00:54:16 -0700

    Add commands to build nginx

--- build.sh
+++ build.sh
@@ -13,6 +13,8 @@ version="1.18.0"
 # set current directory as base directory
 basedir="$(pwd)"
 
-# build docker and copy build artifacts to volume mount
-docker run -it --rm -v "$basedir"/artifacts:/artifacts mcr.microsoft.com/dotnet/core/sdk:3.1-buster /bin/bash -c "`cat scripts/build-fastbin-server-docker.sh`"
+# build fastbin-server and copy build artifacts to volume mount
+docker run -it --rm -v "$basedir"/artifacts:/artifacts mcr.microsoft.com/dotnet/core/sdk:3.1-buster /bin/bash -c "`cat ./scripts/build-fastbin-server-docker.sh`"
 
+# build nginx and copy build artifacts to volume mount
+docker run -it --rm -e "NGINX=$version" -v "$basedir"/artifacts:/build alpine:latest /bin/ash -c "`cat ./scripts/build-nginx-docker.sh`"
