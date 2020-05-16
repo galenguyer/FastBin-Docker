97c6eb6d8800518ed4646860c9832d0996e68daa
Galen Guyer <git@galenguyer.com>
2020-05-16 00:05:39 -0700

    Add nginx build scripts

---
+++ build.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# build, tag, and push docker images
+
+# exit if a command fails
+set -o errexit
+
+# exit if required variables aren't set
+set -o nounset
+
+# first and only argument should be nginx version to build
+version="1.18.0"
+
+# set current directory as base directory
+basedir="$(pwd)"
+
+# build docker and copy build artifacts to volume mount
+docker run -it --rm -e "NGINX=$version" -v "$basedir"/artifacts:/build alpine:latest /bin/ash -c "`cat ./scripts/build-nginx-docker.sh`"
+
+# copy nginx binary to image build directory
+cp "$basedir"/artifacts/nginx-"$version" "$basedir"/image/nginx
+
+# create docker run image
+docker build --build-arg version="$version" -t docker.seedno.de/seednode/nginx:"$version" "$basedir"/image/.
+
+# remove nginx binary from image build directory
+rm "$basedir"/image/nginx
